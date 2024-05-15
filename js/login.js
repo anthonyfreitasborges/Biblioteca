c1637638f77fc9656ee12d3d2bf31d39c8c6e0d7
Marcos Kennedy <marcosnogueira997@gmail.com>
2024-05-15 11:56:14 -0300

    criação da lógica para validar login do usuário

--- js/login.js
+++ js/login.js
@@ -1,3 +1,27 @@
-function validarLogin() {
-    console.log("Olá");
+async function validarLogin() {
+    const url = 'http://localhost:8080/autenticacao/login';
+    const username = document.querySelector('#usuário').value;
+    const password = document.querySelector('#senha').value;
+
+    fetch(url,{
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json',
+        },
+        body: JSON.stringify({username, password}),
+    })
+    .then(response => {
+        if(!response.ok){
+            alert("Credenciais Inválidas");
+            location.reload();
+        }
+        return response.json();
+    })
+    .then(dados => {
+        if(dados) {
+            sessionStorage.setItem('token', dados);
+            window.location.href = '/pages/main.html';
+        }
+
+    })
 }
