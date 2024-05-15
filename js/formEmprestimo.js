cef69c928cb4b3e186721e07f075e1098c11fd72
blackcoffee6681 <williamrodrigueslopess@gmail.com>
2024-05-15 16:08:24 -0300

    criação lógica para registrar e enviar(POST) emprestimo

--- js/formEmprestimo.js
+++ js/formEmprestimo.js
@@ -1,3 +1,61 @@
 function registrarEmprestimo() {
-    const url = 'http://localhost:8080/emprestimos';
+    console.log("olá");
+    const url = "http://localhost:8080/emprestimos";
+
+    document.getElementById("iform").addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const nomeAluno = document.getElementById("nome").value;
+        const nomeLivro = document.getElementById("nomeLivro").value;
+        const dataEmprestimo = document.getElementById("dataEmprestimo").value;
+        const dataDevolucao = document.getElementById("dataDevolucao").value;
+        const salaAluno = document.getElementById("sala").value;
+        const turnoAluno = document.getElementById("turno").value;
+
+        var emprestimo = {
+            aluno: {
+                nome: nomeAluno,
+                turma: salaAluno,
+                turno: turnoAluno
+            },
+            livro: {
+                nomeLivro: nomeLivro
+            },
+            dataEmprestimo: dataEmprestimo,
+            dataDevolucao: dataDevolucao
+        };
+        var token = sessionStorage.getItem('token');
+        console.log(token);
+        fetch(url, {
+            method: "POST",
+            headers: { 
+                "Content-type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify(emprestimo),
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Erro ao enviar os dados!");
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log("Dados enviados com sucesso!", data)
+            })
+            .catch(error => console.error("ERRO", error));
+    })
+}
+
+function recuperarToken(url,options = {}){
+    var token = sessionStorage.getItem("token");
+
+    if(token){
+        if(!options.headers){
+            options.headers = {};
+        }
+        options.headers ['authorization'] = `Bearer ${token}`;
+    }
+
+    return fetch(url,options);
 }
