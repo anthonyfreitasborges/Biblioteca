987381268b8436aee38f018f46f2440cea790eaa
Marcos Kennedy <marcosnogueira997@gmail.com>
2024-06-24 00:52:09 -0300

    atualizado

--- pages/js/formDeleteLivros.js
+++ pages/js/formDeleteLivros.js
@@ -21,8 +21,6 @@ async function deletarLivro() {
 
             const data = await response.json();
             alert(data.mensagem);
-            // Limpa o formulário após a exclusão bem-sucedida
-            document.getElementById('iform-delete-livros').reset();
             location.reload();
         } catch (error) {
             console.error('Erro:', error);
